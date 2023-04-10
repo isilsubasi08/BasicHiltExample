78bed2d8980dce15dcd66ddf68357e3dcb730039
isilsubasi19 <isil.subasi19@gmail.com>
2023-04-10 16:54:49 +0300

    added fragment and package design

---
+++ MVVMRetrofitHiltExample/app/src/main/java/com/isilsubasi/retrofithiltexample/ui/MoviesDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.isilsubasi.retrofithiltexample.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.isilsubasi.retrofithiltexample.R
+
+
+class MoviesDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movies_details, container, false)
+    }
+
+}
